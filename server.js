0deb9f73aa0954bc79de7c162a9837e646b380a6
antoniomacaringue93-ui <antoniomacaringue93@gmail.com>
2026-07-30 23:21:21 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("🚀 Bem-vindo ao ConnectMoz258!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado na porta ${PORT}`);
+});
