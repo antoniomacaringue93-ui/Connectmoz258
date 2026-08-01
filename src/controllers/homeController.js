b9ed471a724708666b9c6981399116c36c9d6543
antoniomacaringue93-ui <antoniomacaringue93@gmail.com>
2026-08-01 19:51:10 +0200

    Create homeController.js

---
+++ src/controllers/homeController.js
@@ -0,0 +1,8 @@
+export const home = (req, res) => {
+  res.json({
+    projeto: "ConnectMoz258",
+    versao: "1.0.0",
+    status: "Sistema Online",
+    mensagem: "Bem-vindo ao ConnectMoz258"
+  });
+};
